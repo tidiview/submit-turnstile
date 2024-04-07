99fe36d3c2aadcad51cfb8d735a9a3ea623af67d
tidiview <francois.vidit@laposte.net>
2024-04-07 21:01:10 +0200

    revert

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -28,8 +28,11 @@ export const onRequestPost = [
 
 		let pretty = JSON.stringify(outcome, null, 2);
 
-		return new Response(await context.env.TURNSTILE.put("key1", pretty)
-		);
+		return new Response(pretty, {
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			}
+		});
 
     })
 
