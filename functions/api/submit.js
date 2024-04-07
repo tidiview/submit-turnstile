ea7c8ee814464de0b5c9e1a3760b2737742310f0
tidiview <francois.vidit@laposte.net>
2024-04-07 20:34:36 +0200

    revert

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -27,8 +27,7 @@ export const onRequestPost = [
 		}
 
 		let pretty = JSON.stringify(outcome, null, 2);
-		const task = await context.env.TURNSTILE.put("turnstile1:", pretty,{
-			metadata: { someMetadataKey: "someMetadataValue" }, expirationTtl: 60});
+		const task = await context.env.TURNSTILE.put("turnstile1:", pretty);
 
 		return new Response(task, {
 			headers: {
